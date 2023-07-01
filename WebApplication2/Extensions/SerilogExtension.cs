52afb86ab951b482a127d663aab71ee85362bacf
vladislav <vgodov02@gmail.com>
2023-07-01 11:37:03 +0300

    added new version logger

---
+++ WebApplication2/Extensions/SerilogExtension.cs
@@ -0,0 +1,24 @@
+﻿using Serilog;
+
+namespace WebApplication2.Extensions;
+
+public static class SerilogExtension
+{
+    public static WebApplicationBuilder AddSerilogExtension(this WebApplicationBuilder builder)
+    {
+        var seqOptions = builder.Configuration.GetSection(nameof(SeqConfig)).Get<SeqConfig>();
+        var logger = new LoggerConfiguration()
+            .WriteTo.Seq(seqOptions.ServerUrl, apiKey: seqOptions.ApiKey)
+            .ReadFrom.Configuration(builder.Configuration)
+            .Enrich.WithProperty("Application", seqOptions.ApiKey)
+            .Enrich.WithProperty("Environment", builder.Environment.EnvironmentName)
+            .CreateLogger();
+
+        Log.Logger = logger;
+
+        builder.Logging.AddSerilog(logger);
+        builder.Host.UseSerilog(logger);
+
+        return builder;
+    }
+}
--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -1,37 +1,28 @@
 using Serilog;
+using Serilog.Extensions.Hosting;
 using WebApplication2;
+using WebApplication2.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var seqOptions = builder.Configuration.GetSection(nameof(SeqConfig)).Get<SeqConfig>();
-var logger = new LoggerConfiguration()
-    .WriteTo.Seq(seqOptions.ServerUrl, apiKey: seqOptions.ApiKey)
-    .ReadFrom.Configuration(builder.Configuration)
-    .Enrich.WithProperty("Application", seqOptions.ApiKey)
-    .Enrich.WithProperty("Environment", builder.Environment.EnvironmentName)
-    .CreateLogger();
-
-Log.Logger = logger;
-
-builder.Logging.AddSerilog(logger);
-
-
+builder.AddSerilogExtension();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseSerilogRequestLogging();
+
 app.UseHttpsRedirection();
+
+
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
